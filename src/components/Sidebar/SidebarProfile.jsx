6e42bf763880a808f1ec388a6c34716428672786
mio95 <raska.mio@gmail.com>
2025-09-24 12:42:11 +0200

    ispisivanje imena ulogovanog korisnika

--- src/components/Sidebar/SidebarProfile.jsx
+++ src/components/Sidebar/SidebarProfile.jsx
@@ -5,19 +5,16 @@ export default function SidebarProfile({ expanded }) {
   const { user } = useSelector((state) => state.auth);
   return (
     <div className="border-t flex p-3">
-      <img
-        src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
-        alt="User"
-        className="w-10 h-10 rounded-md"
-      />
       <div
         className={`flex justify-between items-center overflow-hidden transition-all ${
-          expanded ? "w-52 ml-3" : "w-0"
+          expanded ? "w-full ml-3" : "w-0"
         }`}
       >
         <div className="leading-4">
-          <h4 className="font-semibold">{user.username}</h4>
-          <span className="text-xs text-gray-600">{user.email}</span>
+          <h4 className="font-semibold">
+            {user.firstName} {user.lastName}
+          </h4>
+          <span className="text-xs text-gray-600">{user.role}</span>
         </div>
         <HiDotsVertical size={20} />
       </div>
